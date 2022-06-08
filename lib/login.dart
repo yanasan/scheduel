be4d90b6ca664794c2a9b1eb469961fcc8ca9896
Yanachi <s.yanachi0817@gmail.com>
2022-06-08 22:08:08 +0900

    first commit

---
+++ lib/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
